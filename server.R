94c562fa594f5562ff6ed39f57fbea3fab797e17
gsverhoeven <gertjan.verhoeven@gmail.com>
2023-07-14 14:04:29 +0200

    insert DKDLOB tabs obv initiele vragen

--- server.R
+++ server.R
@@ -1,39 +1,81 @@
 server <- function(input, output, session) {
+  source("determine_cp.R", local = TRUE)
+  source("generate_radio_buttons.R", local = TRUE)
   
-  source("dkdl_model.R", local = TRUE)
-  source("casemix_vragen.R", local = TRUE)
-  
-  radios_initieel <- casemix_vragen(filename = "casemix_vragenlijst_full.csv", 
+  radios_initieel <- generate_radio_buttons(filename = "casemix_vragenlijst_full.csv", 
                                     subset = "Initiële vragen")
   
   output$radios_initieel <- renderUI(radios_initieel) # Renders reactive HTML using the Shiny UI library.
 
-  radios_draagkracht <- casemix_vragen(filename = "casemix_vragenlijst_full.csv", 
+  radios_draagkracht <- generate_radio_buttons(filename = "casemix_vragenlijst_full.csv", 
                                     subset = "Draagkracht")
   
   output$radios_draagkracht <- renderUI(radios_draagkracht) # Renders reactive HTML using the Shiny UI library.  
 
-  radios_draaglast <- casemix_vragen(filename = "casemix_vragenlijst_full.csv", 
+  radios_draaglast <- generate_radio_buttons(filename = "casemix_vragenlijst_full.csv", 
                                     subset = "Draaglast")
   
   output$radios_draaglast <- renderUI(radios_draaglast) # Renders reactive HTML using the Shiny UI library.
   
-  radios_ob <- casemix_vragen(filename = "casemix_vragenlijst_full.csv", 
+  radios_ob <- generate_radio_buttons(filename = "casemix_vragenlijst_full.csv", 
                                     subset = "Ondersteuningsbehoefte")
   
   output$radios_ob <- renderUI(radios_ob) # Renders reactive HTML using the Shiny UI library.
   
   output$cm_txt_output <- renderText({
-    # hier eerst check functie aanroepen: zijn alle benodigde vragen beantwoord?
-    if(!(determine_cp(input) %in% c(-1, NULL))){
-      paste("De gekozen antwoordopties leiden af naar cliëntprofiel <br> <b>", determine_cp(input), "</b>")}
-    else {paste("Nog niet alle vragen zijn beantwoord")}
+    shiny::req(input$Q1, input$Q2, input$Q3)
+    paste("De gekozen antwoordopties leiden af naar cliëntprofiel: <br> <br> <Br> <h1> <b>", determine_cp(input), "</b> </h1")
   })
 
   observeEvent(input$jumpToP2, {
+    if(req(input$Q1)){
+      if (input$Q1 == "1") {
+        insertTab(inputId = "navbar", 
+                  tab = tabPanel(
+                    title = "Draagkracht",
+                    value = "draagkracht",
+                    uiOutput("radios_draagkracht"),
+                    actionButton('jumpToP3', 'Ga naar Draaglast', class = "btn-lg btn-success"),
+                    p("")
+                  ),
+                  target = "initieel")
+        insertTab(inputId = "navbar", 
+                  tab = tabPanel(
+                    title = "Draaglast",
+                    value = "draaglast",
+                    uiOutput("radios_draaglast"),
+                    actionButton('jumpToP4', 'Ga naar Ondersteuningsbehoefte', class = "btn-lg btn-success"),
+                    p("")
+                  ),
+                  target = "draagkracht")
+        insertTab(inputId = "navbar", 
+                  tab = tabPanel(
+                    title = "Ondersteuningsbehoefte",
+                    value = "ondersteuningsbehoefte",
+                    uiOutput("radios_ob"),
+                    actionButton('jumpToP5', 'Ga naar Cliëntprofiel', class = "btn-lg btn-success"),
+                    p("")
+                  ),
+                  target = "draaglast")           
+      }
+      if(input$Q1 == "2" | is.null(input$Q1)) {
+        removeTab(inputId = "navbar", target = "draagkracht")
+        removeTab(inputId = "navbar", target = "draaglast")
+        removeTab(inputId = "navbar", target = "ondersteuningsbehoefte")
+      }
+    }
+    print(input$draagkracht)
+    if(!is.null(input$draagkracht)){
+      
     updateTabsetPanel(session, "navbar",
                       selected = "draagkracht")
+    } else{
+      updateTabsetPanel(session, "navbar",
+                        selected = "clientprofiel")
+    }
+    print(input$draagkracht)
   })
+  
   observeEvent(input$jumpToP3, {
     updateTabsetPanel(session, "navbar",
                       selected = "draaglast")
@@ -44,7 +86,7 @@ server <- function(input, output, session) {
   })
   observeEvent(input$jumpToP5, {
     updateTabsetPanel(session, "navbar",
-                      selected = "results")
+                      selected = "clientprofiel")
   })
   observeEvent(input$jumpToP1, {
     updateTabsetPanel(session, "navbar",
