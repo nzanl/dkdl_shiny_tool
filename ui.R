39901c00c328afea303206704b8ad947249b5ff3
gsverhoeven <gertjan.verhoeven@gmail.com>
2023-07-14 14:04:46 +0200

    DKDLOB panels optioneel activeren

--- ui.R
+++ ui.R
@@ -5,34 +5,13 @@ ui <- navbarPage(
     title = "Initiële vragen",
     value = "initieel",
     uiOutput("radios_initieel"),
-    actionButton('jumpToP2', 'Ga naar Draagkracht', class = "btn-lg btn-success"),
+    actionButton('jumpToP2', 'Volgende stap', class = "btn-lg btn-success"),
     p("")
   ),
-  tabPanel(
-    title = "Draagkracht",
-    value = "draagkracht",
-    uiOutput("radios_draagkracht"),
-    actionButton('jumpToP3', 'Ga naar Draaglast', class = "btn-lg btn-success"),
-    p("")
-  ),  
-  tabPanel(
-    title = "Draaglast",
-    value = "draaglast",
-    uiOutput("radios_draaglast"),
-    actionButton('jumpToP4', 'Ga naar Ondersteuningsbehoefte', class = "btn-lg btn-success"),
-    p("")
-  ),
-  tabPanel(
-    title = "Ondersteuningsbehoefte",
-    value = "ondersteuningsbehoefte",
-    uiOutput("radios_ob"),
-    actionButton('jumpToP5', 'Ga naar Cliëntprofiel', class = "btn-lg btn-success"),
-    p("")
-  ),    
   tabPanel(
     title = "Cliëntprofiel",
-    value = "results",
-    textOutput("cm_txt_output"),
+    value = "clientprofiel",
+    htmlOutput("cm_txt_output"),
     actionButton('jumpToP1', 'Ga terug naar de vragenlijst')
   ),
   tabPanel(
